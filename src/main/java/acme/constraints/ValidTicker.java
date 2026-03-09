a3ddb97b807f8085c637264ec8d81ba1fd1dbbd5
albcarsic <albcarsic@alum.us.es>
2026-03-09 19:44:19 +0100

    feat: Añadir constraints grupales

---
+++ src/main/java/acme/constraints/ValidTicker.java
@@ -0,0 +1,29 @@
+package acme.constraints;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotBlank;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@ReportAsSingleViolation
+
+@NotBlank
+@Pattern(regexp = "^[A-Z]{2}[0-9]{2}-\\w{5,10}$")
+
+public @interface ValidTicker {
+    // Standard validation properties -----------------------------------------
+
+	String message() default "{acme.validation.ticker.message}";
+
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
